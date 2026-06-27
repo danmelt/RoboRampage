a3fad5d64090f9cfbbee85afa5ca399b1f59c294
Dan <danmelt@gmail.com>
2026-06-27 16:19:25 -0400

    initial commit

---
+++ Weapons/hit_scan_weapon.gd
@@ -0,0 +1,34 @@
+extends Node3D
+@export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh : Node3D
+@export var weapon_damage := 15
+@export var muzzle_flash : GPUParticles3D
+
+@onready var cool_down_timer: Timer = $CoolDownTimer
+@onready var weapon_position: Vector3 = weapon_mesh.position
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if cool_down_timer.is_stopped():
+			shoot()
+	
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
+
+
+func shoot() -> void:
+	muzzle_flash.restart()
+	cool_down_timer.start(1.0 / fire_rate)
+	var collider = ray_cast_3d.get_collider()
+	printt("Weapon fired! :", collider)
+	weapon_mesh.position.z += recoil
+	if collider is Enemy:
+		collider.hitpoints -= weapon_damage
